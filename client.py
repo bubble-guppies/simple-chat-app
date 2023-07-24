9657cb33dd40e2f7ce24eb05abf54422382496fd
Charlotte <charlottefreedman9898@gmail.com>
2023-07-24 14:33:40 -0400

    initialized client class

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-import numpy as np
 import uuid
+import socket
 
 class Client:
     '''
@@ -10,13 +10,25 @@ class Client:
     password = ""
     uuid = ""
 
-    def Client(self):
-        pass
-
-    def Client(self, username, ip_address, password, uuid):
+    def client(self, username, ip_address, password, uuid):
         self.username = username
         self.ip_address = ip_address
-        self.password = password
+        self._password = password
         self.uuid = uuid
+    
+    def get_username(self):
+        return self.username
+    
+    def get_ip_address(self):
+        return self.ip_address
+    
+    def get_uuid(self):
+        return self.uuid
+    
+    def send_message(self):
+        pass
+
+    def disconnect(self):
+        pass
 
     
